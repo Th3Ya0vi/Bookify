9d202651df200ff1f46f54661d1ff5aa8ecf61ab
npat1126 <neal.patel1126@gmail.com>
2016-03-23 14:50:09 -0500

    Popular VC implemented
    User can now retrieve books that have been previously posted.

---
+++ Bookify/Bookify/PopularBooksCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PopularBooksCollectionViewCell.swift
+//  Bookify
+//
+//  Created by Neal Patel on 3/23/16.
+//  Copyright © 2016 Bookify. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class PopularBooksCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var popularBooksImageView: UIImageView!
+
+    var picture: PFFile? {
+        didSet {
+            print(picture)
+            picture?.getDataInBackgroundWithBlock({ (data: NSData?, error: NSError?) -> Void in
+                if error == nil {
+                    self.popularBooksImageView.image = UIImage(data: data!)!
+                }
+                else {
+                    print(error?.localizedDescription)
+                    print("ERROR")
+                }
+            })
+        }
+    }
+}
